dc2c0d0095e76ed8cce8a5f8ad293e7e4b7c1daf
Jaehyeong Park <78674944+JaehyeongPark06@users.noreply.github.com>
2022-07-06 14:07:09 -0400

    Update gradeclass.cpp

--- Week 5/gradeclass.cpp
+++ Week 5/gradeclass.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+  int grades1;
+  int grades2;
+  
+  cout << "Please enter 2 grades, seperated by a space: " << endl;
+  cin >> grades1 >> grades2;
+
+  if (grades1 < 60 & grades2 < 60)
+    cout << "Student Failed:(" << endl;
+
+  else if (grades1 >= 95 & grades2 >= 95)
+    cout << "Student Graduated with Honors:)" << endl;
+
+  else
+    cout << "Student Graduated!" << endl;
+  
+  return 0;
+}
